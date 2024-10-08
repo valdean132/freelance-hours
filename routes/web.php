7ccb0f3fe3c91c29f0fb30952bce87461148aea2
Valdean Souza <65917804+valdean132@users.noreply.github.com>
2024-10-08 15:44:54 -0400

    Iniciando projeto, Criando os Models User e Projects.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', WelcomeController::class);
+Route::get('/counter', Counter::class);
