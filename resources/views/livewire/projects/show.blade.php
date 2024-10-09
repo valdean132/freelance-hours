4f727ec930b59cdb3316dacf96cf4204f1cabf8b
Valdean Souza <65917804+valdean132@users.noreply.github.com>
2024-10-09 12:34:21 -0400

    Criando Livewire Components

---
+++ resources/views/livewire/projects/show.blade.php
@@ -0,0 +1,8 @@
+<div>
+    Compomente LiveWire Projects.Show
+
+    <pre>
+        Title: {{ $project->title }}
+        Description: {!! $project->description !!}
+    </pre>
+</div>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <livewire:projects.show :$project />
+    <livewire:projects.proposals :$project />
+</div>
